ede9b05cdad2c257db65761e7a1427849a189036
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2026-02-07 15:59:22 -0500

    feat(fetchInitialPokemonData): Function that fetches initial pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const pokemonNames = ["pikachu", "charizard", "bulbasaur", "charmander", "squirtle", "eevee", "jigglypuff", "gengar", "snorlax", "lapras", "dragonite", "mewtwo"];
+  const [initialPokemonData, setInitialPokemonData] = useState([]);
+  const [exampleName, setExampleName] = useState(null);
+  const pokemonDataFetched = useRef(false);
+
+  useEffect(() => {
+    if (!pokemonDataFetched.current) {
+      const fetchPokemonData = async () => {
+        const initialFetchedPokemonData = [];
+        try {
+          for (let i = 0; i < 12; i++) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNames[i]}`);
+            const pokemonData = await response.json();
+            initialFetchedPokemonData.push({
+              id: pokemonData.id,
+              name: pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1),
+              imgageUrl: pokemonData.sprites.other["official-artwork"]["front_default"],
+            });
+            pokemonDataFetched.current = true;
+          }
+        } catch (error) {
+          alert("There was an error");
+          console.error(error.message);
+        }
+
+        console.log("initialFetchedPokemonData", initialFetchedPokemonData);
+        setInitialPokemonData(initialFetchedPokemonData);
+      };
+
+      fetchPokemonData();
+
+      //
+    }
+  }, []);
+
+  const [randomizedPokemon, setRandomizedPokemon] = useState([]);
+  const [currentScore, setCurrentScore] = useState([]);
+  const [bestScore, setBestScore] = useState([]);
+  const [currentRoundClickedPokemon, setCurrentRoundClickedPokemon] = useState([]);
+  const handlePokemonCardClick = () => {};
+
+  //console.log("initialPokemonData.current", initialPokemonData.current);
+
+  function randomizePokemonOrder() {
+    const randomizedPokemon = [];
+    const usedIndexNumbers = [];
+    console.log("initialPokemonData.current Indy", initialPokemonData.current.length);
+    for (let i = 1; i < 13; i++) {
+      let randomIndexNumber = Math.floor(Math.random() * 12);
+      while (usedIndexNumbers.includes(randomIndexNumber)) {
+        randomIndexNumber = Math.floor(Math.random() * 12);
+      }
+
+      console.log("randomIndexNumber", randomIndexNumber);
+
+      randomizedPokemon.push(initialPokemonData.current);
+    }
+    console.log("randomizedPokemon", randomizedPokemon);
+  }
+  console.log("pokeoe", pokemonDataFetched);
+
   return (
     <>
       <header>
@@ -13,7 +74,7 @@ function App() {
       </header>
       <main className="gameboard">
         <div className="pokemon-card">
-          <span>Pika Pika</span>
+          <span>{pokemonDataFetched.current && initialPokemonData[3].name}</span>
           <img src="/public/images/favicon.png" alt="" />
         </div>
         <div className="pokemon-card">
